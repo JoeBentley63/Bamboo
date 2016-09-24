b238e1016b04b36a587f45682227e86107a0ca66
Student <Student>
2016-09-24 14:18:25 +0100

    ball and throwing

--- src/scripts/Ball.cs
+++ src/scripts/Ball.cs
@@ -3,13 +3,48 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 
+	public float maxSpeed;
+	public Vector2 originalScale;
+	public Vector2 currentScale;
+	private Rigidbody2D ballRigidBody;
 	// Use this for initialization
 	void Start () {
-	
+		ballRigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+		originalScale = this.transform.localScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		CapMaxSpeed();
+	}
+
+	public void Caught(Transform parent){
+		ballRigidBody.isKinematic = true;
+		ballRigidBody.GetComponent<Collider2D>().enabled = false;
+
+		this.transform.SetParent(parent); 
+	}
+
+	public void Release(Vector2 releaseVelocity){
+		ballRigidBody.isKinematic = false;
+		ballRigidBody.GetComponent<Collider2D>().enabled = true;
+		this.transform.SetParent(null);
+		ballRigidBody.velocity = releaseVelocity * 2;
+	}
+
+	void CapMaxSpeed(){
+		if(ballRigidBody.velocity.x > maxSpeed){
+			ballRigidBody.velocity = new Vector2(maxSpeed, ballRigidBody.velocity.y);
+		}
+		else if(ballRigidBody.velocity.x < -maxSpeed){
+			ballRigidBody.velocity = new Vector2(-maxSpeed, ballRigidBody.velocity.y);
+		}
+
+		if(ballRigidBody.velocity.y > maxSpeed){
+			ballRigidBody.velocity = new Vector2(ballRigidBody.velocity.x, maxSpeed);
+		}
+		else if(ballRigidBody.velocity.y < -maxSpeed){
+			ballRigidBody.velocity = new Vector2(ballRigidBody.velocity.x, -maxSpeed);
+		}
 	}
 }
--- src/scripts/player.cs
+++ src/scripts/player.cs
@@ -3,27 +3,36 @@ using System.Collections;
 
 public class player : MonoBehaviour {
 
+	public GameObject ball;
 	public Color color;
 	public float runVelocity;
 	public float jumpVelocity;
 	public float dashVelocity;
 	public float maxSpeed;
+	public float ballPullModifier;
+	public float ballAttractionRadius;
 	public LayerMask layerMask;
 	private Rigidbody2D playerRigidbody;
-	public bool canDoubleJump = true;
+	private Rigidbody2D ballRigidbody;
+	public bool canDoubleJump = false;
 	public bool IsTouchingGround = false;
+	public bool tryingToCatchBall = false;
+
+	private Ball ballScript;
+
 	// Use this for initialization
 	void Start () {
 		playerRigidbody = this.gameObject.GetComponent<Rigidbody2D>();
+		ballRigidbody = ball.gameObject.GetComponent<Rigidbody2D>();
+		ballScript = ball.gameObject.GetComponent<Ball>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		IsTouchingGround = touchingGround();
+		IsTouchingGround = TouchingGround();
 		if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)){
 			//playerRigidbody.velocity = new Vector2(0, playerRigidbody.velocity.y);
 		}
-		playerRigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal") * runVelocity, 0, 0));
 
 		if(Input.GetKey(KeyCode.DownArrow)){
 			playerRigidbody.AddForce(new Vector3(0, -dashVelocity, 0));
@@ -38,11 +47,35 @@ public class player : MonoBehaviour {
 			}
 		}
 
+		playerRigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal") * runVelocity, 0, 0));
+
 		CapMaxSpeed();
+		CatchBall();
 	}
 
+	void CatchBall(){
+		tryingToCatchBall = Input.GetKey(KeyCode.LeftControl);
+		Vector2 differenceBetweenPlayerAndBallPos = (playerRigidbody.transform.position - ball.transform.position);
+	
+		if(tryingToCatchBall && differenceBetweenPlayerAndBallPos.magnitude < ballAttractionRadius){
+			ballRigidbody.AddForce(differenceBetweenPlayerAndBallPos * ballPullModifier);
+		}
 
-	bool touchingGround(){
+		if(Input.GetKeyDown(KeyCode.LeftAlt)) ballScript.Release(playerRigidbody.velocity);
+	}
+
+	void OnTriggerEnter2D(Collider2D collision){
+		if(tryingToCatchBall && collision.gameObject == ball){
+			ballScript.Caught(this.transform);
+			ball.transform.position = this.transform.position + this.transform.up;
+		}
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.DrawWireSphere(this.gameObject.transform.position, ballAttractionRadius);
+	}
+
+	bool TouchingGround(){
 		RaycastHit2D hit = Physics2D.Raycast(playerRigidbody.transform.position, -playerRigidbody.transform.up, 0.7f, layerMask);
 
 		if (hit.collider) canDoubleJump = true;
